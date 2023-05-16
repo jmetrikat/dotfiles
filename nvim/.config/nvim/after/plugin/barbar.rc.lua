e26e83bdbe0d1d056c6070237c2a33ce1b715980
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-05-16 15:51:35 +0200

    Fix nvim bufferline error temporarily

--- nvim/.config/nvim/after/plugin/barbar.rc.lua
+++ nvim/.config/nvim/after/plugin/barbar.rc.lua
@@ -1,5 +1,12 @@
-vim.cmd[[
-let bufferline.animation = v:false
-let bufferline.auto_hide = v:true
-]]
+require('bufferline').setup{
+    options = {
+        animation = false,
+        auto_hide = true,
+    }
+}
 
+-- caused an error for bufferline
+-- vim.cmd[[
+-- let bufferline.animation = v:false
+-- let bufferline.auto_hide = v:true
+-- ]]
